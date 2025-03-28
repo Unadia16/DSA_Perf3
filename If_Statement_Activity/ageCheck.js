efc27284031dbfd2d3d9bcdb48a89fbea7c3353e
Unadia16 <unadiaaarumexander@gmail.com>
2025-03-28 12:00:19 +0800

    Add files via upload

---
+++ If_Statement_Activity/ageCheck.js
@@ -0,0 +1,12 @@
+//UNADI, AARUME XANDER BSCS-1B
+//If Statement Activity
+let age = prompt("Enter your age my friend:");//Creating a program that prompts the user to enter their age.
+
+//Using an if-else structure to determine if the user is.
+if(age < 13) {
+    console.log("Your still a Child.");
+}else if (age >= 13 && age <= 19) {
+    console.log("You are already a Teenager.");
+}else{
+    console.log("You are already an Adult");
+}
