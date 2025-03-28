380ac0ad4d5cb5c573be8b7e7e16ba251be00571
Unadia16 <unadiaaarumexander@gmail.com>
2025-03-28 12:06:32 +0800

    Add files via upload

---
+++ For_Loop_Activity/evenNumbers.js
@@ -0,0 +1,7 @@
+//UNADI, AARUME XANDER BSCS-1B
+//For Loop Activity
+ for (let i = 0; i <= 10; i++) {
+    if (i % 2 === 0){//Modifying the program to print only even numbers between 1 and 10.
+        console.log(i)
+    }
+ }
